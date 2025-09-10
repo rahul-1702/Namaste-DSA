8c0d998cc4935fa9d06b1cfc775722c41373e6f4
Rahul-taak <rtaak786@gmail.com>
2025-09-11 00:02:36 +0530

    remove duplicate done

--- Problems/Arrays/remove_duplicate.js
+++ Problems/Arrays/remove_duplicate.js
@@ -1,13 +1,14 @@
 function removeDuplicates(nums){
-
+    const arr = [];
     for(let i = 0; i < nums.length; i++){
-        console.log("i = ", i);
+        if(nums[i + 1] !== nums[i]){
+            arr.push(nums[i]);
+        }
     }
-
+    return arr;
 }
 
-let arr = [1, 1,2, 2, 2, 3, 3, 4, 5, 5, 5];
-
+let arr = [1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 5,5, 6, 6];
 // output: [1, 2, 3, 4, 5]
 
 console.log(removeDuplicates(arr));
