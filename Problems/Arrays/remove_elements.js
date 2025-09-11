6225b989a439ebe5ebd49e5365dc0fbd752e5ec6
Rahul-taak <rtaak786@gmail.com>
2025-09-11 22:38:57 +0530

    resolved

--- Problems/Arrays/remove_elements.js
+++ Problems/Arrays/remove_elements.js
@@ -1,22 +1,16 @@
 function removeElements(nums, k) {
-
-    let x = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i - 1] !== nums[i]){
-            if(nums[i] === k){
-                x = i;
-            }else{
-                nums[x] = nums[i];
-                x = x + 1;
-            }
-        }    
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== k) {
+      nums[x] = nums[i];
+      x = x + 1;
     }
+  }
 
-    return nums;
-
+  return nums;
 }
 
-const arr = [6, 8, 3, 2, 2, 5, 0, 4, 2, 2, 9];
+const arr = [6, 8, 2, 0, 4, 2, 2, 2, 8, 2, 2, 2, 2, 2, 2, 4];
 const element = 2;
 
 console.log(removeElements(arr, element));
