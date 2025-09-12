39e4b47f01943f7ff941887ec2ce0f485e7cb807
Rahul-taak <rtaak786@gmail.com>
2025-09-12 23:47:56 +0530

    best time buy sell

--- Problems/Scenario_based/best_time_buy_sell_stock.js
+++ Problems/Scenario_based/best_time_buy_sell_stock.js
@@ -1,2 +1,18 @@
 // Code to Best Time Time to Buy Sell Stock
 
+function bestTimeBuySellStock(price) {
+  let diff = 0;
+  for (let x = 0; x < price.length; x++) {
+    for (let i = x; i < price.length; i++) {
+      if (price[i] > price[x] && price[i] - price[x] > diff) {
+        diff = price[i] - price[x];
+      }
+    }
+  }
+
+  return diff;
+}
+
+const arr = [7, 1, 5, 8, 6, 4];
+
+console.log(bestTimeBuySellStock(arr));
