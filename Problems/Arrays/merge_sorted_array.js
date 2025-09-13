5bfe43694de07faaa97c9a38b43d74f2daf830ee
Rahul-taak <rtaak786@gmail.com>
2025-09-13 23:38:56 +0530

    merge sorted ARRAY

--- Problems/Arrays/merge_sorted_array.js
+++ Problems/Arrays/merge_sorted_array.js
@@ -10,11 +10,31 @@ function mergeSortedArrays(n1, n2){
             x++;
         }
     }
+    return n1.sort();
+}
+
+function optimizeSolution(n1, m, n2, n){
+    let n1Copy = n1.slice(0, m);
+
+    let p1 = 0;
+    let p2 = 0;
+    for(let i = 0; i < m+n; i++){
+       
+        if(p2 >= n || (p1 < m && n1Copy[p1] < n2[p2])){
+            n1[i] = n1Copy[p1];
+            p1++;
+        }else{
+            n1[i] = n2[p2];
+            p2++;
+        }
+        
+    }
+
     return n1;
 }
 
-
-const nums1 = [1, 2, 0, 0, 0, 0, 0];
-const nums2 = [2, 5, 6, 8, 4];
+const nums1 = [1, 2, 8, 3, 0, 0, 0];
+const nums2 = [2, 5, 6, 3, 4];
 
 console.log(mergeSortedArrays(nums1, nums2));
+console.log(optimizeSolution(nums1, nums1.length, nums2, nums2.length));
