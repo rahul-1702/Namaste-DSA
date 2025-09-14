1058c84a0cd341daf44e3b192779753cc6aa3bfd
Rahul-taak <rtaak786@gmail.com>
2025-09-14 13:21:21 +0530

    solved issue

--- Problems/Arrays/merge_sorted_array.js
+++ Problems/Arrays/merge_sorted_array.js
@@ -1,26 +1,30 @@
 // Code to Merge Sorted Arrays
 
-function mergeSortedArrays(n1, n2){
-    let x = -1;
-    for(let i = 0; i < n1.length; i++){
-        if(n1[i] === 0 && x === -1){
-            x = 0;
-        }
-        if(x >= 0){
-            n1[i] = n2[x];
-            x++;
-        }
-    }
-    return n1.sort();
-}
+// function mergeSortedArrays(n1, n2){
+//     let x = -1;
+//     for(let i = 0; i < n1.length; i++){
+//         if(n1[i] === 0 && x === -1){
+//             x = 0;
+//         }
 
-function optimizeSolution(n1, m, n2, n){
+//         if(x >= 0){
+//             n1[i] = n2[x];
+//             x++;
+//         }
+//     }
+//     return n1.sort();
+// }
+
+function optimizedSolution(n1, n2){
+    let m = n1.filter(x => x !== 0).length;
+    let n = n2.length;
+
     let n1Copy = n1.slice(0, m);
 
     let p1 = 0;
     let p2 = 0;
     for(let i = 0; i < m+n; i++){       
-        if(p2 >= n || (p1 < m && n1Copy[p1] < n2[p2])){
+        if(p1 < m && n1Copy[p1] < n2[p2]){
             n1[i] = n1Copy[p1];
             p1++;
         }else{
@@ -31,8 +35,8 @@ function optimizeSolution(n1, m, n2, n){
     return n1;
 }
 
-const nums1 = [1, 2, 8, 3, 0, 0, 0];
-const nums2 = [2, 5, 6, 3, 4];
+const nums1 = [1, 2, 7, 8, 0, 0, 0, 0, 0];
+const nums2 = [2, 4, 5, 5, 9];
 
-console.log(mergeSortedArrays(nums1, nums2));
-console.log(optimizeSolution(nums1, nums1.length, nums2, nums2.length));
+// console.log(mergeSortedArrays(nums1, nums2));
+console.log(optimizedSolution(nums1, nums2));
