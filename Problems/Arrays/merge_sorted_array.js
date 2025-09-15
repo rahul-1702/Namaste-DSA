7a0aecb9c4b3485e2d34b9198917d5dc790e5811
Rahul-taak <rtaak786@gmail.com>
2025-09-15 22:33:09 +0530

    second optimized solutionnode /home/rahul/RAHUL/VSCode/NamasteDSA/Problems/Arrays/merge_sorted_array.js

--- Problems/Arrays/merge_sorted_array.js
+++ Problems/Arrays/merge_sorted_array.js
@@ -15,28 +15,50 @@
 //     return n1.sort();
 // }
 
-function optimizedSolution(n1, n2){
-    let m = n1.filter(x => x !== 0).length;
-    let n = n2.length;
+// function optimizedSolution(n1, n2) {
+//   let m = n1.filter((x) => x !== 0).length;
+//   let n = n2.length;
 
-    let n1Copy = n1.slice(0, m);
+//   let n1Copy = n1.slice(0, m);
 
-    let p1 = 0;
-    let p2 = 0;
-    for(let i = 0; i < m+n; i++){       
-        if(p1 < m && n1Copy[p1] < n2[p2]){
-            n1[i] = n1Copy[p1];
-            p1++;
-        }else{
-            n1[i] = n2[p2];
-            p2++;
-        }
+//   let p1 = 0;
+//   let p2 = 0;
+//   for (let i = 0; i < m + n; i++) {
+//     if (p1 < m && n1Copy[p1] < n2[p2]) {
+//       n1[i] = n1Copy[p1];
+//       p1++;
+//     } else {
+//       n1[i] = n2[p2];
+//       p2++;
+//     }
+//   }
+//   return n1;
+// }
+
+function secondOptimizedSolution(n1, n2) {
+  let m = n1.filter((x) => x !== 0).length;
+  let n = n2.length;
+  let size = n1.length;
+
+  let n1Copy = n1.slice(0, m);
+
+  let p1 = m - 1;
+  let p2 = n - 1;
+  for (let i = 1; i < m + n; i++) {
+    if(p2 < 0 || (p1 >= 0 && n1Copy[p1]>n2[p2])){
+        n1[size - i] = n1Copy[p1];
+        p1--;
+    }else{
+        n1[size - i] = n2[p2];
+        p2--;
     }
-    return n1;
+  }
+  return n1;
 }
 
-const nums1 = [1, 2, 7, 8, 0, 0, 0, 0, 0];
-const nums2 = [2, 4, 5, 5, 9];
+const nums1 = [1, 3, 4, 6, 9, 0, 0, 0, 0, 0];
+const nums2 = [2, 5, 6, 8, 9];
 
 // console.log(mergeSortedArrays(nums1, nums2));
-console.log(optimizedSolution(nums1, nums2));
+// console.log(optimizedSolution(nums1, nums2));
+console.log(secondOptimizedSolution(nums1, nums2));
