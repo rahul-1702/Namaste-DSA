bd2c18d5e0eba6d2902654c14304499262dd4bd2
Rahul-taak <rtaak786@gmail.com>
2025-09-16 21:25:03 +0530

    optimmized

--- Problems/Arrays/missing_number.js
+++ Problems/Arrays/missing_number.js
@@ -1,21 +1,34 @@
 // Find Missing number in array
 
-function missingNumber(arr){
-    let temp = [];
+// function missingNumber(arr){
+//     let temp = [];
 
-    for(let i = 0; i < arr.length; i++){   
-        temp[arr[i]] = 1;
-    }
+//     for(let i = 0; i < arr.length; i++){
+//         temp[arr[i]] = 1;
+//     }
 
-    for(let j = 0; j <= temp.length; j++){
-        if(temp[j] !== 1){
-            return j;
-        }
-    }
+//     for(let j = 0; j <= temp.length; j++){
+//         if(temp[j] !== 1){
+//             return j;
+//         }
+//     }
 
-    return 0;
+//     return 0;
+// }
+
+function optimizedSolution(arr) {
+  let n = arr.length;
+  let sum = (n * (n + 1)) / 2;
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    count = count + arr[i];
+  }
+
+  return sum - count;
 }
 
-const nums = [2, 0, 1];
+const nums = [4, 0, 2, 1, 5];
 
-console.log(missingNumber(nums));
+// console.log(missingNumber(nums));
+console.log(optimizedSolution(nums));
