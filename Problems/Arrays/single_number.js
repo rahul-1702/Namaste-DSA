e2e5f7307533dcf53fbe149fe1454c3eb691f0b6
Rahul-taak <rtaak786@gmail.com>
2025-09-16 21:34:37 +0530

    single number array

---
+++ Problems/Arrays/single_number.js
@@ -0,0 +1,25 @@
+// Find one Single Number in array
+
+function singleNumber(arr) {
+  let temp = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (temp[arr[i]] === 1) {
+      temp[arr[i]] = temp[arr[i]] + 1;
+    } else {
+      temp[arr[i]] = 1;
+    }
+  }
+
+  for (let j = 0; j < temp.length; j++) {
+    if (temp[j] === 1) {
+      return j;
+    }
+  }
+
+  return -1;
+}
+
+const nums = [2, 2, 4, 5, 5, 1, 5, 1];
+
+console.log(singleNumber(nums));
