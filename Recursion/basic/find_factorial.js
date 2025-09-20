fae181a004dce5f582a6a8fb807953282ab093f4
Rahul-taak <rtaak786@gmail.com>
2025-09-20 14:25:09 +0530

    factorial done

---
+++ Recursion/basic/find_factorial.js
@@ -0,0 +1,13 @@
+// num = 5 = 120
+
+function findFactorial(num) {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * findFactorial(num - 1);
+}
+
+let num = 5;
+
+console.log(findFactorial(num));
