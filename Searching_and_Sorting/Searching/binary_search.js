fa0ab23c6d185827dc3581316afc786001a8a1ce
Rahul-taak <rtaak786@gmail.com>
2025-09-20 19:42:33 +0530

    two ways binary search done

--- Searching_and_Sorting/Searching/binary_search.js
+++ Searching_and_Sorting/Searching/binary_search.js
@@ -2,26 +2,43 @@
 // let target = 7;
 // output => 3
 
-function binarySearch(arr, target, start, end) {
-  let mid = Math.floor((start + end) / 2);
+function binarySearchLoop(arr, target, start, end) {
+  while (end >= start) {
+    let mid = Math.floor((start + end) / 2);
 
-  if (start > end) return -1;
+    if (arr[mid] === target) return mid;
 
-  if (arr[mid] === target) return mid;
-
-  if (target > arr[mid]) {
-    start = mid + 1;
-    return binarySearch(arr, target, start, end);
-  } else {
-    end = mid - 1;
-    return binarySearch(arr, target, start, end);
+    if (target > arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
   }
+
+  return -1;
 }
 
+// function binarySearchRecursion(arr, target, start, end) {
+//   let mid = Math.floor((start + end) / 2);
+
+//   if (start > end) return -1;
+
+//   if (arr[mid] === target) return mid;
+
+//   if (target > arr[mid]) {
+//     start = mid + 1;
+//     return binarySearchRecursion(arr, target, start, end);
+//   } else {
+//     end = mid - 1;
+//     return binarySearchRecursion(arr, target, start, end);
+//   }
+// }
+
 let nums = [2, 4, 5, 7, 8, 9];
-let target = 7;
+let target = 2;
 
 let start = 0;
 let end = nums.length - 1;
 
-console.log(binarySearch(nums, target, start, end));
+console.log(binarySearchLoop(nums, target, start, end));
+// console.log(binarySearchRecursion(nums, target, start, end));
