1261163c763f3532c6e05fd365fe718f23bb3118
Rahul-taak <rtaak786@gmail.com>
2025-09-20 12:56:41 +0530

    finalise

--- Recursion/array_problems/sum_of_all_elements.js
+++ Recursion/array_problems/sum_of_all_elements.js
@@ -8,7 +8,6 @@
 //     return arr[0] + sumOfAllElements(arr.splice(1))
 // }
 
-let marks = [6, 8, 3, 2, 5, 1, 9, 0, 4, 7];
 
 function sumOfAllElements2(n){
     if(n === 0){
@@ -18,6 +17,6 @@ function sumOfAllElements2(n){
     return marks[n] + sumOfAllElements2(n - 1)
 }
 
-
+let marks = [6, 8, 3, 2, 5, 1, 9, 0, 4, 7];
 // console.log(sumOfAllElements(marks));
 console.log(sumOfAllElements2(marks.length - 1));
