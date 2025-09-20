4153689c20ef12002fe105811ba0f2d0e191eb75
Rahul-taak <rtaak786@gmail.com>
2025-09-20 12:36:56 +0530

    sum of all elements in array

---
+++ Recursion/array_problems/sum_of_all_elements.js
@@ -0,0 +1,13 @@
+// [6, 8, 3, 2, 5, 1, 9, 0, 4, 7] = 45
+
+function sumOfAllElements(arr){
+    if(arr.length === 0){
+        return 0;
+    }
+
+    return arr[0] + sumOfAllElements(arr.splice(1))
+}
+
+let marks = [6, 8, 3, 2, 5, 1, 9, 0, 4, 7];
+
+console.log(sumOfAllElements(marks));
