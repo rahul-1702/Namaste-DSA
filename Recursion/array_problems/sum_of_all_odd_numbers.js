c55e40cfab9a5c85c442d9274078c70c3a3021f8
Rahul-taak <rtaak786@gmail.com>
2025-09-20 13:14:04 +0530

    second SOlution done

--- Recursion/array_problems/sum_of_all_odd_numbers.js
+++ Recursion/array_problems/sum_of_all_odd_numbers.js
@@ -1,17 +1,30 @@
 // [2, 7, 3, 9, 6, 8, 5, 4] = 24
 
-function sumOfAllOddNumbers(arr) {
-  if (arr.length === 0) {
+// function sumOfAllOddNumbers(arr) {
+//   if (arr.length === 0) {
+//     return 0;
+//   }
+
+//   if (arr[0] % 2 === 1) {
+//     return arr[0] + sumOfAllOddNumbers(arr.splice(1));
+//   } else {
+//     return 0 + sumOfAllOddNumbers(arr.splice(1));
+//   }
+// }
+
+function sumOfAllOddNumbers2(n) {
+  if (n === 0) {
     return 0;
   }
 
-  if (arr[0] % 2 === 1) {
-    return arr[0] + sumOfAllOddNumbers(arr.splice(1));
+  if (marks[n] % 2 === 1) {
+    return marks[n] + sumOfAllOddNumbers2(n - 1);
   } else {
-    return 0 + sumOfAllOddNumbers(arr.splice(1));
+    return 0 + sumOfAllOddNumbers2(n - 1);
   }
 }
 
 let marks = [2, 7, 3, 9, 6, 8, 5, 4];
 
-console.log(sumOfAllOddNumbers(marks));
+// console.log(sumOfAllOddNumbers(marks));
+console.log(sumOfAllOddNumbers2(marks.length - 1));
