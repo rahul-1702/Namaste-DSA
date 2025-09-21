1c2f53c6ec1a57907961e69b1506cc429a878b47
Rahul-taak <rtaak786@gmail.com>
2025-09-21 14:21:03 +0530

    selection sort

---
+++ Searching_and_Sorting/Sorting/selection_sort.js
@@ -0,0 +1,23 @@
+// nums = [5, 2, 4, 1, 3]
+// output = [1, 2, 3, 4, 5]
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+
+  return arr;
+}
+
+let nums = [5, 2, 4, 1, 3];
+
+console.log(selectionSort(nums));
