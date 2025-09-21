745d5f4fcf43aa810b964c2d417888b0b29e751b
Rahul-taak <rtaak786@gmail.com>
2025-09-21 19:36:59 +0530

    hint added

--- Searching_and_Sorting/Sorting/bubble_sort.js
+++ Searching_and_Sorting/Sorting/bubble_sort.js
@@ -1,5 +1,6 @@
 // nums = [5, 2, 4, 1, 3]
 // output = [1, 2, 3, 4, 5]
+// Move bigger to end by swapping
 
 function bubbleSort(arr) {
   for (let i = 1; i < arr.length; i++) {
--- Searching_and_Sorting/Sorting/insertion_sort.js
+++ Searching_and_Sorting/Sorting/insertion_sort.js
@@ -1,5 +1,6 @@
 // nums = [5, 2, 4, 1, 3]
 // output = [1, 2, 3, 4, 5]
+// Think while Inserting, Select any
 
 function selectionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
