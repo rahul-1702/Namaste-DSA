34ba3e0bf0705cf28dcbe0441c5de3ba9f1f0b9c
Rahul-taak <rtaak786@gmail.com>
2025-09-21 13:16:21 +0530

    bubble sort optimized

--- Searching_and_Sorting/Sorting/bubble_sort.js
+++ Searching_and_Sorting/Sorting/bubble_sort.js
@@ -3,16 +3,19 @@
 
 function bubbleSort(arr) {
   for (let i = 1; i < arr.length; i++) {
+    let sorted = true;
     for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j + 1];
-        arr[j + 1] = arr[j];
-        arr[j] = temp;
+        arr[j] = arr[j] + arr[j + 1];
+        arr[j + 1] = arr[j] - arr[j + 1];
+        arr[j] = arr[j] - arr[j + 1];
+        sorted = false;
       }
     }
+    if (sorted) {
+      return arr;
+    }
   }
-
-  return arr;
 }
 
 let nums = [5, 2, 4, 1, 3];
