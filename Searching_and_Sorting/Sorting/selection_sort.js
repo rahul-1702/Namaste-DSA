fa5a8c4891e1015ea1bb0c243d2f76e66cec3f97
Rahul-taak <rtaak786@gmail.com>
2025-09-21 14:47:02 +0530

    optimized selection sort

--- Searching_and_Sorting/Sorting/selection_sort.js
+++ Searching_and_Sorting/Sorting/selection_sort.js
@@ -4,7 +4,7 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let min = i;
-    for (let j = i; j < arr.length; j++) {
+    for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[min]) {
         min = j;
       }
