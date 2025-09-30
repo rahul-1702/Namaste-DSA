36efc57c9be68cb62c122c6fbe7d282fb9072f37
Rahul-taak <rtaak786@gmail.com>
2025-09-30 23:31:18 +0530

    progrma for cehck palindome in array

--- Problems/Arrays/palindrome_array.js
+++ Problems/Arrays/palindrome_array.js
@@ -1,1 +1,16 @@
 // program for check palindrome of array
+
+function checkPalindrome(arr) {
+  for (let i = 0; i < Math.floor(nums.length / 2); i++) {
+    let j = nums.length - i - 1;
+    if (nums[i] !== nums[j]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let nums = [1, 2, 3, 2, 1];
+
+console.log(checkPalindrome(nums));
