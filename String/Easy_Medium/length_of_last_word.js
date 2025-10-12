c5bbb43da173c223ede890a610ba752cfbddf70a
Rahul-taak <rtaak786@gmail.com>
2025-10-12 12:32:51 +0530

    length of last word

---
+++ String/Easy_Medium/length_of_last_word.js
@@ -0,0 +1,28 @@
+function lengthOfLastWord(str) {
+  let s = str.trim();
+  let n = s.length;
+  let count = 0;
+
+  while (n >= 0 && s[n] !== " ") n--;
+
+  while (n < s.length - 1) {
+    count++;
+    n++;
+  }
+
+  return count;
+}
+
+function lengthOfLastWordOptimized(str) {
+  let s = str.trim();
+  let n = s.length;
+  let count = 0;
+
+  while (n >= 0 && s[n--] !== " ") count++;
+
+  return --count;
+}
+
+let str = " Welcome to my world ";
+console.log(lengthOfLastWord(str));
+console.log(lengthOfLastWordOptimized(str));
