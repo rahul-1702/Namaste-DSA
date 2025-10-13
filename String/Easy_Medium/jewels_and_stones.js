e03e8f794124a2e7c1a471a685626bb66791e9b4
Rahul-taak <rtaak786@gmail.com>
2025-10-13 22:35:48 +0530

    done

--- String/Easy_Medium/jewels_and_stones.js
+++ String/Easy_Medium/jewels_and_stones.js
@@ -1,1 +1,27 @@
 // Jewels and Stones
+
+function JewelsAndStones(jew, stn) {
+  let count = 0;
+  for (let i = 0; i < jew.length; i++) {
+    for (let j = 0; j < stn.length; j++) {
+      if (jew[i] === stn[j]) count++;
+    }
+  }
+
+  return count;
+}
+
+function JewelsAndStonesOptimized(jew, stn) {
+  let count = 0;
+  for (let i = 0; i < stn.length; i++) {
+    if (jew.includes(stn[i])) count++;
+  }
+
+  return count;
+}
+
+let jewels = "aA";
+let stones = "aAAbbbb";
+
+console.log(JewelsAndStones(jewels, stones));
+console.log(JewelsAndStonesOptimized(jewels, stones));
