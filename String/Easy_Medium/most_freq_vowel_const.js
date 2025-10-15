2d43b23e19188b6dc77416ac611c58c3e2a781d2
Rahul-taak <rtaak786@gmail.com>
2025-10-15 23:14:18 +0530

    freq added

--- String/Easy_Medium/most_freq_vowel_const.js
+++ String/Easy_Medium/most_freq_vowel_const.js
@@ -1,1 +1,31 @@
 // Find most frequent vowels and constants
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+  const freq = {};
+  
+  // count frequencies
+  for (let ch of s) {
+    freq[ch] = (freq[ch] || 0) + 1;
+  }
+
+  let vMax = 0; // max vowel frequency
+  let cMax = 0; // max consonant frequency
+
+  for (let ch in freq) {
+    if ("aeiou".includes(ch)) {
+      vMax = Math.max(vMax, freq[ch]);
+    } else if (/[a-z]/.test(ch)) { // consonant check
+      cMax = Math.max(cMax, freq[ch]);
+    }
+  }
+
+  return vMax + cMax;
+};
+
+let str = "successes";
+
+console.log(maxFreqSum(str));
