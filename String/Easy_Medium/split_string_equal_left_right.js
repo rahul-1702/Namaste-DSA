fb7e0bbe545c539595c3453813dcfd14256eeeab
Rahul-taak <rtaak786@gmail.com>
2025-10-19 14:02:23 +0530

    split strging added

---
+++ String/Easy_Medium/split_string_equal_left_right.js
@@ -0,0 +1,27 @@
+function splitLeftRight(str) {
+  let cL = 0;
+  let cR = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "L") {
+      cL++;
+    } else {
+      cR++;
+    }
+
+    if (cL === cR) {
+      count++;
+      cL = 0;
+      cR = 0;
+    }
+  }
+
+  return count;
+}
+
+let str1 = "LLLRRRLRRL";
+let str2 = "LRLLRRLRLR";
+
+console.log(splitLeftRight(str1));
+console.log(splitLeftRight(str2));
