7389c666ce8e01922caa7d1f0355d0e5f08979b9
Rahul-taak <rtaak786@gmail.com>
2025-10-19 14:14:49 +0530

    optimized

--- String/Easy_Medium/split_string_equal_left_right.js
+++ String/Easy_Medium/split_string_equal_left_right.js
@@ -20,8 +20,24 @@ function splitLeftRight(str) {
   return count;
 }
 
+function splitLeftRightOptimized(str) {
+  let check = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "L") check++;
+    else check--;
+
+    if (check === 0) count++;
+  }
+
+  return count;
+}
+
 let str1 = "LLLRRRLRRL";
 let str2 = "LRLLRRLRLR";
 
 console.log(splitLeftRight(str1));
 console.log(splitLeftRight(str2));
+console.log("Optimized :", splitLeftRightOptimized(str1));
+console.log("Optimized :", splitLeftRightOptimized(str2));
