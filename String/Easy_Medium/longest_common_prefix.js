a8fd6efb6972b87aec74a8ea1bcf29323876d176
Rahul-taak <rtaak786@gmail.com>
2025-10-26 23:10:38 +0530

    lcp

---
+++ String/Easy_Medium/longest_common_prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let s = strs.reduce((a, b) => (a.length < b.length ? a : b));
+  let l = "";
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    for (let str of strs) {
+      if (str[i] !== c) return l;
+    }
+    l += c;
+  }
+  return l;
+}
+
+let strs = ["flower", "flast", "floor"];
+
+console.log(longestCommonPrefix(strs));
