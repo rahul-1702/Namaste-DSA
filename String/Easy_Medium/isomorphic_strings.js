3563596ebea1904ad3b3a1107bdfb1a7b273e673
Rahul-taak <rtaak786@gmail.com>
2025-10-26 23:17:10 +0530

    isomorphic string

---
+++ String/Easy_Medium/isomorphic_strings.js
@@ -0,0 +1,20 @@
+function isIsomorphic(s, t) {
+  let mapSToT = {};
+  let mapTToS = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!mapSToT[s[i]] && !mapTToS[t[i]]) {
+      mapSToT[s[i]] = t[i];
+      mapTToS[t[i]] = s[i];
+    } else if (mapTToS[t[i]] !== s[i] || mapSToT[s[i]] !== t[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let s = "egg";
+let t = "add";
+
+console.log(isIsomorphic(s, t));
