52fbe4789b7ba73d76443d4776d3856dd1edd4bd
Rahul-taak <rtaak786@gmail.com>
2025-11-10 00:36:51 +0530

    without stack also done

---
+++ Stack_Queue/Problems/rm_outer_paranthesis_simple.js
@@ -0,0 +1,25 @@
+
+var removeOuterParentheses = function(s) {
+    let level = -1;
+    let ans = "";
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            ++level;
+            ans += (level ? s[i] : "");
+        } else {
+            ans += (level ? s[i] : "");
+            --level;
+        }
+    }
+    return ans;
+};
+
+
+console.log(removeOuterParentheses("(()())(())")); 
+// Output: "()()()"
+
+console.log(removeOuterParentheses("(()())(())(()(()))")); 
+// Output: "()()()()(())"
+
+console.log(removeOuterParentheses("()()")); 
+// Output: ""
