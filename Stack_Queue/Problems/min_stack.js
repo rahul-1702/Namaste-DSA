10b1ddf325a3cb5c95b1d6eed5b56157b9e0a29e
Rahul-taak <rtaak786@gmail.com>
2025-11-09 23:47:06 +0530

    space added

--- Stack_Queue/Problems/min_stack.js
+++ Stack_Queue/Problems/min_stack.js
@@ -1,6 +1,7 @@
 var MinStack = function() {
     this.s = [];
 };
+
 MinStack.prototype.push = function(val) {
     if(this.s.length === 0){
         this.s.push([val, val]);
@@ -9,12 +10,15 @@ MinStack.prototype.push = function(val) {
         this.s.push([val, minVal]);
     }
 };
+
 MinStack.prototype.pop = function() {
     this.s.pop();
 };
+
 MinStack.prototype.top = function() {
     return this.s[this.s.length-1][0];
 };
+
 MinStack.prototype.getMin = function() {
     return this.s[this.s.length-1][1];
 };
