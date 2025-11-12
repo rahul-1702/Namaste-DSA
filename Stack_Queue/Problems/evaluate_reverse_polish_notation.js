751f5b810484ba0bc13fa01fc5c32159e1cf2b5e
Rahul-taak <rtaak786@gmail.com>
2025-11-12 23:06:07 +0530

    code added

--- Stack_Queue/Problems/evaluate_reverse_polish_notation.js
+++ Stack_Queue/Problems/evaluate_reverse_polish_notation.js
@@ -1,1 +1,35 @@
 // Evaluate Reverse Polish Notation
+
+var evalRPN = function(arr) {
+    let stack = [];
+    const map = {
+        "+": (a,b) => (b+a),
+        "*": (a,b) => (b*a),
+        "-": (a,b) => (b-a),
+        "/": (a,b) => Math.trunc(b/a),
+    };
+        for(let i=0; i < arr.length; i++){
+            if(map[arr[i]]) {
+                let a = stack.pop();
+                let b = stack.pop();
+                let ans = map[arr[i]](+a,+b);
+                stack.push(ans);
+            } else {
+                stack.push(arr[i]) 
+            }
+        }
+    return Number(stack.pop());
+};
+
+// ✅ Test Cases
+console.log(evalRPN(["2", "1", "+", "3", "*"]));    
+// (2 + 1) * 3 = 9
+
+console.log(evalRPN(["4", "13", "5", "/", "+"]));   
+// 13 / 5 = 2 (truncated), so 4 + 2 = 6
+
+console.log(evalRPN(["3", "11", "+", "5", "-"]));   
+// (3 + 11) - 5 = 9
+
+console.log(evalRPN(["5", "1", "2", "+", "4", "*", "+", "3", "-"]));  
+// 5 + ((1 + 2) * 4) - 3 = 14
