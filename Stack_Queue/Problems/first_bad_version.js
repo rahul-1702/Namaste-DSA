dc0cdb6d398cd6e87852f42a35d36d4f45b83611
Rahul-taak <rtaak786@gmail.com>
2025-11-26 23:09:23 +0530

    simple files added

---
+++ Stack_Queue/Problems/first_bad_version.js
@@ -0,0 +1,16 @@
+var solution = function(isBadVersion) {
+    return function(n) {
+        let l = 1;
+        let r = n;
+        while (l < r) {
+            let m = l + Math.floor((r - l) / 2);
+            if (!isBadVersion(m)) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return r;
+    };
+};
+
