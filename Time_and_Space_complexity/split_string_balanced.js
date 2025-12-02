245da95b5ba97aca59299f9e38451a84bd42142b
Rahul-taak <rtaak786@gmail.com>
2025-12-02 22:38:07 +0530

    split string into balanced

---
+++ Time_and_Space_complexity/split_string_balanced.js
@@ -0,0 +1,19 @@
+// Split string in balanced string
+
+function BalancedString(str) {
+  let check = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "L") check++;
+    else check--;
+
+    if (check === 0) count++;
+  }
+
+  return count;
+}
+
+let str = "LLLLRRRRLLLRRRLRRLLLRRRL";
+
+console.log(BalancedString(str));
